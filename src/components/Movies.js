2c25e9f5ea97fcba86cc74cb3216fcb8ccd5ba87
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-09 03:16:47 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -6,6 +6,13 @@ const Movies = () => {
     <div>
         {/*{code here}*/}
         <h1>Movies Page</h1>
+        {movies.map(movie =>
+          <div key={movie.title}>
+            <h1>Name: {movie.title}</h1>
+            <p>Time: {movie.time}</p>
+            <p>Genres:</p>
+            <ul>{movie.genres.map(genre => <li key={genre}>{genre}</li>)}</ul>
+          </div>)}
     </div>
   );
 };
