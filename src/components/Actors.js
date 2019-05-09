62a606d3a41979ed3f01e21ecc0e166562cea230
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-09 03:07:55 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -6,6 +6,12 @@ const Actors = () => {
     <div>
       {/*{code here}*/}
       <h1>Actors Page</h1>
+      {actors.map(actor =>
+          <div>
+            <h1>Name: {actor.name}</h1>
+            <p>Movies:</p>
+            <ul>{actor.movies.map(movie => <li>{movie}</li>)}</ul>
+          </div>)}
     </div>
   );
 };
