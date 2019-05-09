acb426bd534dc615d6caf1e12b7a71537dbf1611
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-09 03:17:51 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -15,8 +15,7 @@ const NavBar = () => {
   return (
     <div>
       {/*{code here}*/ /* add the navbar component */}
-      <NavLink
-        to="/"
+      <NavLink to="/"
         /* set exact so it knows to only set activeStyle when route is deeply equal to link */
         exact
         /* add styling to Navlink */
